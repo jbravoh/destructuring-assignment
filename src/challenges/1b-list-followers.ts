6138d8f9ccec3f8a70d94825345c563cae019fe7
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-08-06 17:22:21 +0100

    Use destructuring assignment

--- src/challenges/1b-list-followers.ts
+++ src/challenges/1b-list-followers.ts
@@ -1,7 +1,6 @@
 function listFollowers(followerNames: string[]): string {
-  // complete function
-  // use array destructuring, for practice!
-  return "fails test right now";
+  const [nameOne, nameTwo] = followerNames;
+  return `Followed by ${nameOne}, ${nameTwo} and others`;
 }
 
 export default listFollowers;
